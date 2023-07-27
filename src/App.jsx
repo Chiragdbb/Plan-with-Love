e596aaa4dd6760124c2db639815ef895f52562a3
Chiragdbb <schirag414@gmail.com>
2023-07-27 18:42:15 +0530

    finished setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-5xl'>Working</div>
+    <div className='text-5xl'></div>
   )
 }
 
